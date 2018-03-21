5927edad40d62727a374fb22eb9cae5b9628cbcf
houlin <306118788@qq.com>
2018-03-21 18:36:00 +0800

    去除无用方法

--- app/src/main/java/com/houlin/capstone_project/MyApplication.java
+++ app/src/main/java/com/houlin/capstone_project/MyApplication.java
@@ -1,9 +1,7 @@
 package com.houlin.capstone_project;
 
 import android.app.Application;
-import android.content.Context;
 import android.os.Handler;
-import android.os.Looper;
 
 import com.google.android.gms.ads.MobileAds;
 import com.houlin.capstone_project.utils.ToastUtils;
@@ -28,16 +26,4 @@ public class MyApplication extends Application {
         return handler;
     }
 
-    public static boolean isRunOnUIThread() {
-        Looper myLooper = Looper.myLooper();
-        return myLooper == Looper.getMainLooper();
-    }
-
-    public static void runOnUIThread(Runnable runnable) {
-        if (isRunOnUIThread()) {
-            runnable.run();
-        } else {
-            handler.post(runnable);
-        }
-    }
 }
